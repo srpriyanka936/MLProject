d8e41f7bb8227d10663799b5f926d42bb8aaa478
Priyanka SR <srpriyanka936@gmail.com>
2026-02-10 21:35:13 +0530

    app

--- src/exception.py
+++ src/exception.py
@@ -19,10 +19,14 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-#try:
- #   a = 1 / 0
-#except Exception as e:
- #   raise CustomException(e, sys)
+#if __name__=="__main__":
+ #   try:
+    #  a = 1 / 0
+ #   except Exception as e:
+  #    logging.info("divide by zero error")
+  #    raise CustomException(e, sys)
+  
+
 
     
 
